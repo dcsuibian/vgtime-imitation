85d2992fce9e8206db208a09895f10d1a976b96c
dcsuibian <dcsuibian@163.com>
2023-10-06 21:54:15 +0800

    新建SpringBoot 3的backend

---
+++ backend/src/test/java/com/dcsuibian/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcsuibian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
