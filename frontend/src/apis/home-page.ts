3a32ccf8ecca591d6c46e7eaa5bcf91de80ac16f
dcsuibian <dcsuibian@163.com>
2023-12-06 19:10:25 +0800

    适配Docker

--- frontend/src/apis/home-page.ts
+++ frontend/src/apis/home-page.ts
@@ -1,7 +1,9 @@
 import { HomePage } from '@/types'
 
+const { BACKEND_API_BASE_URL } = process.env
+
 export async function getHomePage(): Promise<HomePage> {
-  const res = await fetch('http://localhost:9528/api/home-page')
+  const res = await fetch(`${BACKEND_API_BASE_URL}/home-page`)
   if (!res.ok) {
     throw new Error('请求失败')
   }
--- frontend/src/apis/topic.ts
+++ frontend/src/apis/topic.ts
@@ -1,7 +1,9 @@
 import { Topic } from '@/types'
 
+const { BACKEND_API_BASE_URL } = process.env
+
 export async function getTopicById(id: number): Promise<Topic> {
-  const res = await fetch(`http://localhost:9528/api/topics/${id}`)
+  const res = await fetch(`${BACKEND_API_BASE_URL}/topics/${id}`)
   if (!res.ok) {
     throw new Error('请求失败')
   }
