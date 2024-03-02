15018928fe923cd20566ed65bc7bca2e0f47319a
dcsuibian <dcsuibian@163.com>
2024-03-03 01:40:19 +0800

    frontend-admin增加.env文件，导入常用包，配置开发服务器和别名

--- frontend-admin/vite.config.ts
+++ frontend-admin/vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 9526,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:9528',
+        changeOrigin: true,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
