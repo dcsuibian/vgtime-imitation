7420ee57f9e9bb6a7d107140b0da39ae24c67204
dcsuibian <dcsuibian@163.com>
2024-03-03 00:08:13 +0800

    frontend微调

--- frontend/src/components/Avatar/index.tsx
+++ frontend/src/components/Avatar/index.tsx
@@ -1,5 +1,5 @@
 import styles from './index.module.scss'
-import { User } from '@/types'
+import type { User } from '@/types'
 
 export default function Avatar({ user }: { user: User }) {
   const avatarSrc = user.avatar ?? 'https://img01.vgtime.com//image/tou.gif'
